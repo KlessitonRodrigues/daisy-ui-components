182a3aa91cab454c0fac17b6b8a18c102467b412
klessiton <klessitom@gmail.com>
2026-04-05 07:14:48 -0300

    chore: dark theme background

--- components/i18n/RedirectPage.tsx
+++ components/i18n/RedirectPage.tsx
@@ -8,6 +8,8 @@ import { getDefaultLanguage } from '../../utils/html';
 export const RedirectPage = () => {
   useEffect(() => {
     const currentUrl = new URL(window.location.href);
+    console.log(currentUrl);
+
     const lang = getDefaultLanguage();
     redirect(`/${lang}${currentUrl.pathname}`);
   }, []);
