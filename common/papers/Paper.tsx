406f4fcfcf68608ed6c0eaee5d07fc380a3fb5bb
klessiton <klessitom@gmail.com>
2026-04-07 06:58:21 -0300

    feat: update card style

--- common/papers/Paper.tsx
+++ common/papers/Paper.tsx
@@ -5,7 +5,7 @@ interface PaperProps extends HTMLAttributes<HTMLDivElement> {}
 
 export const Paper = (props: PaperProps) => {
   const { className, children, ...paperProps } = props;
-  const classNames = ['paper  w-full space-y-4 rounded-md bg-bg1 p-4 shadow-sm slide-up'];
+  const classNames = ['paper w-full space-y-4 rounded-sm bg-bg1 p-4 shadow-sm slide-up'];
 
   return (
     <div className={twMerge(...classNames, className)} {...paperProps}>
